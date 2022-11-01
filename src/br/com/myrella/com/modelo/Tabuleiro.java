d8f625fa7172a7e98d5b9db80b4c485e0f40d6fd
myrellauchoa <myrellauchoast@gmail.com>
2022-11-01 18:49:12 -0300

    first commit

---
+++ src/br/com/myrella/com/modelo/Tabuleiro.java
@@ -0,0 +1,5 @@
+package br.com.myrella.com.modelo;
+
+public class Tabuleiro {
+
+}
---
+++ test/br/com/myrella/visao/CampoTeste.java
@@ -0,0 +1,56 @@
+package br.com.myrella.visao;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import br.com.myrella.com.modelo.Campo;
+
+public class CampoTeste {
+
+	private Campo campo;
+	
+	@BeforeEach
+	void startup() {
+		campo = new Campo(3, 3);
+	}
+	
+	@Test
+	void testeVizinhoDistanciaEsquerda() {
+		Campo vizinho = new Campo(3, 2);
+		boolean resultado = campo.adicionarVizinho(vizinho);
+		assertTrue(resultado);
+	}
+	
+	@Test
+	void testeVizinhoDistanciDireita() {
+		Campo vizinho = new Campo(3, 4);
+		boolean resultado = campo.adicionarVizinho(vizinho);
+		assertTrue(resultado);
+	}
+	
+	@Test
+	void testeVizinhoDistanciaEmCima() {
+		Campo vizinho = new Campo(2, 3);
+		boolean resultado = campo.adicionarVizinho(vizinho);
+		assertTrue(resultado);
+	}
+	
+	@Test
+	void testeVizinhoDistanciaEmbaixo() {
+		Campo vizinho = new Campo(4, 3);
+		boolean resultado = campo.adicionarVizinho(vizinho);
+		assertTrue(resultado);
+	}
+	
+	@Test
+	void testaNaoVizinho() {
+		Campo vizinho = new Campo(1, 1);
+		boolean resultado = campo.adicionarVizinho(vizinho);
+		assertFalse(resultado);
+	}
+	
+	
+}
